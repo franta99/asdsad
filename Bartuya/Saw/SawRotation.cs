e3f788868183eaf961bf6fab44921ddf9febf2ba
Furkan <frknkznc@gmail.com>
2019-01-21 19:09:50 +0300

    Saw

---
+++ Bartuya/Saw/SawRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawRotation : MonoBehaviour
+{
+
+	float sawSpeed = 150f;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.Rotate(0, 0, 1 * Time.deltaTime * sawSpeed);
+	}
+}
